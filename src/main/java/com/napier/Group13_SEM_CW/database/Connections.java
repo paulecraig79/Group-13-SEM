06c3c0ea0e1ca11da32e500f82d9ad29daa91b6e
Daniel van Dijke <40508135@live.napier.ac.uk>
2022-03-17 16:34:10 +0000

    Use server connection instead of local

--- src/main/java/com/napier/Group13_SEM_CW/database/Connections.java
+++ src/main/java/com/napier/Group13_SEM_CW/database/Connections.java
@@ -33,7 +33,7 @@ public class Connections{
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:33060/world?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 return con;
             }
