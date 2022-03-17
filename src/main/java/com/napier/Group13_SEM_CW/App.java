463bd9ad72bf221e35cabe41fa65d6e20319c429
Daniel van Dijke <40508135@live.napier.ac.uk>
2022-03-17 17:22:00 +0000

    All city report methods moved into report.Cities class as opposed to App

--- src/main/java/com/napier/Group13_SEM_CW/App.java
+++ src/main/java/com/napier/Group13_SEM_CW/App.java
@@ -27,9 +27,9 @@ public class App
 
         //ArrayList<City> cities10 = city.getTopCitiesInContinent("Asia", 10, con);
 
-        ArrayList<City> cities11 = city.getTopCitiesInRegion("Southeast Asia", 10, con);
+        ArrayList<City> cities11 = city.getTopCitiesInDistrict("England", 10, con);
 
-        a.printCities(cities11);
+        city.printCities(cities11);
 
         //a.printCapitals(cities2);
 
@@ -39,123 +39,6 @@ public class App
         c.disconnect();
 
 
-    }
-
-    /** Use case 11. Gets the top N populated cities in a region where N is provided by the user.
-     *
-     * @param region
-     * @param limit
-     * @return An array list of the N most populated cities in a region.
-     */
-//    public ArrayList<City> getTopCitiesInRegion(String region, int limit)
-//    {
-//        try {
-//            // Create an SQL statement
-//            Statement stmt = con.createStatement();
-//            // Create string for SQL statement
-//            String strSelect =
-//                    "SELECT city.name, country.name AS country, district, city.population " +
-//                            "FROM city JOIN country ON (code = city.countrycode) " +
-//                            "WHERE region = '" + region + "'" +
-//                            " ORDER BY population DESC " +
-//                            " LIMIT " + limit + ";";
-//            // Execute SQL statement
-//            ResultSet rset = stmt.executeQuery(strSelect);
-//            // Return city if valid.
-//            // Check one is returned.
-//            ArrayList<City> cities = new ArrayList<>();
-//            while (rset.next()) {
-//                City city1 = new City();
-//                city1.name = rset.getString("city.name");
-//                city1.countrycode = rset.getString("country");
-//                city1.district = rset.getString("district");
-//                city1.population = rset.getInt("city.population");
-//                cities.add(city1);
-//            }
-//            return cities;
-//        } catch (Exception e) {
-//            System.out.println(e.getMessage());
-//            System.out.println("Didn't manage to get city details");
-//            return null;
-//        }
-//    }
-
-    /** Use case 12. Gets the top N populated cities in a country where N is provided by the user.
-     *
-     * @param country
-     * @param limit
-     * @return An array list of the N most populated cities in a country.
-     */
-    public ArrayList<City> getTopCitiesInCountry(String country, int limit)
-    {
-        try {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String strSelect =
-                    "SELECT city.name, country.name AS country, district, city.population " +
-                            "FROM city JOIN country ON (code = city.countrycode) " +
-                            "WHERE country.name = '" + country + "'" +
-                            " ORDER BY population DESC " +
-                            " LIMIT " + limit + ";";
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Return city if valid.
-            // Check one is returned.
-            ArrayList<City> cities = new ArrayList<City>();
-            while (rset.next()) {
-                City city1 = new City();
-                city1.name = rset.getString("city.name");
-                city1.countrycode = rset.getString("country");
-                city1.district = rset.getString("district");
-                city1.population = rset.getInt("city.population");
-                cities.add(city1);
-            }
-            return cities;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("Didn't manage to get city details");
-            return null;
-        }
-    }
-
-    /** Use case 13. Gets the top N populated cities in a district where N is provided by the user.
-     *
-     * @param district
-     * @param limit
-     * @return An array list of the N most populated cities in a district.
-     */
-    public ArrayList<City> getTopCitiesInDistrict(String district, int limit)
-    {
-        try {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String strSelect =
-                    "SELECT city.name, country.name AS country, district, city.population " +
-                            "FROM city JOIN country ON (code = city.countrycode) " +
-                            "WHERE district = '" + district + "'" +
-                            " ORDER BY population DESC " +
-                            " LIMIT " + limit + ";";
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Return city if valid.
-            // Check one is returned.
-            ArrayList<City> cities = new ArrayList<>();
-            while (rset.next()) {
-                City city1 = new City();
-                city1.name = rset.getString("city.name");
-                city1.countrycode = rset.getString("country");
-                city1.district = rset.getString("district");
-                city1.population = rset.getInt("city.population");
-                cities.add(city1);
-            }
-            return cities;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("Didn't manage to get city details");
-            return null;
-        }
     }
 
     /** Use case 14. Gets all capital cities in the world by organised from largest to smallest
@@ -259,33 +142,6 @@ public class App
             System.out.println(e.getMessage());
             System.out.println("Didn't manage to get capital city details");
             return null;
-        }
-    }
-
-    /**
-     * Prints the contents specified of a list of countries.
-     *
-     * @param cities
-     */
-    public void printCities(ArrayList<City> cities)
-    {
-        // Check cities is not null
-        if (cities == null)
-        {
-            System.out.println("No cities");
-            return;
-        }
-        // Print header
-        System.out.println(String.format("%-30s %-14s %-20s %-8s", "name", "country", "district", "population"));
-        // Loop over all countries in the list
-        for (City city : cities)
-        {
-            if (city == null)
-                continue;
-            String emp_string =
-                    String.format("%-30s %-14s %-20s %-8s",
-                            city.name, city.countrycode, city.district, city.population);
-            System.out.println(emp_string);
         }
     }
 
--- src/main/java/com/napier/Group13_SEM_CW/reports/Cities.java
+++ src/main/java/com/napier/Group13_SEM_CW/reports/Cities.java
@@ -1,7 +1,6 @@
 package com.napier.Group13_SEM_CW.reports;
 
 import com.napier.Group13_SEM_CW.City;
-import com.napier.Group13_SEM_CW.App;
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -121,6 +120,72 @@ public class Cities {
             System.out.println(e.getMessage());
             System.out.println("Didn't manage to get city details");
             return null;
+        }
+    }
+
+    /** Use case 13. Gets the top N populated cities in a district where N is provided by the user.
+     *
+     * @param district
+     * @param limit
+     * @return An array list of the N most populated cities in a district.
+     */
+    public ArrayList<City> getTopCitiesInDistrict(String district, int limit, Connection con)
+    {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT city.name, country.name AS country, district, city.population " +
+                            "FROM city JOIN country ON (code = city.countrycode) " +
+                            "WHERE district = '" + district + "'" +
+                            " ORDER BY population DESC " +
+                            " LIMIT " + limit + ";";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return city if valid.
+            // Check one is returned.
+            ArrayList<City> cities = new ArrayList<>();
+            while (rset.next()) {
+                City city1 = new City();
+                city1.name = rset.getString("city.name");
+                city1.countrycode = rset.getString("country");
+                city1.district = rset.getString("district");
+                city1.population = rset.getInt("city.population");
+                cities.add(city1);
+            }
+            return cities;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Didn't manage to get city details");
+            return null;
+        }
+    }
+
+    /**
+     * Prints the contents specified of a list of countries.
+     *
+     * @param cities
+     */
+    public void printCities(ArrayList<City> cities)
+    {
+        // Check cities is not null
+        if (cities == null)
+        {
+            System.out.println("No cities");
+            return;
+        }
+        // Print header
+        System.out.println(String.format("%-30s %-14s %-20s %-8s", "name", "country", "district", "population"));
+        // Loop over all countries in the list
+        for (City city : cities)
+        {
+            if (city == null)
+                continue;
+            String emp_string =
+                    String.format("%-30s %-14s %-20s %-8s",
+                            city.name, city.countrycode, city.district, city.population);
+            System.out.println(emp_string);
         }
     }
 }
