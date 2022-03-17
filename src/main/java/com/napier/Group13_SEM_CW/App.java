a2e55f40948bc9b4673b9e3d28e8999d504faa9b
Amy Macdonald <40508596@live.napier.ac.uk>
2022-03-17 15:32:29 +0000

    The top N populated capital cities in the world where N is provided by the user.

--- src/main/java/com/napier/Group13_SEM_CW/App.java
+++ src/main/java/com/napier/Group13_SEM_CW/App.java
@@ -2,7 +2,7 @@ package com.napier.Group13_SEM_CW;
 
 
 import java.sql.*;
-
+import java.util.ArrayList;
 
 
 public class App
@@ -14,6 +14,16 @@ public class App
 
         // Connect to database
         a.connect();
+
+        //ArrayList<City> capitals = a.topPopulatedCapitalsInWorld();
+
+        //ArrayList<City> city = a.livingInCityLivingOutCity();
+
+        //ArrayList<City> cities = a.topPopulatedCapitalsInContinent();
+
+        //a.printCities(capitals);
+
+       // a.printCapitals(city);
 
         // Disconnect from database
         a.disconnect();
@@ -85,4 +95,96 @@ public class App
                 }
             }
         }
+
+
+    /** Use case 17. Gets all capital cities in the world by organised from largest to smallest
+     *
+     * @return An array list of the capital cities in the world in order of desceding population.
+     **/
+    public ArrayList<City> topPopulatedCapitalsInWorld()
+    {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT city.name, city.countrycode, city.population " +
+                            "FROM city JOIN country ON (code = city.countrycode) " +
+                            " ORDER BY population DESC LIMIT 10;";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return city if valid.
+            // Check one is returned.
+            ArrayList<City> capitals = new ArrayList<>();
+            while (rset.next()) {
+                City capital1 = new City();
+                capital1.name = rset.getString("city.name");
+                capital1.countrycode = rset.getString("city.countrycode");
+                capital1.population = rset.getInt("city.population");
+                capitals.add(capital1);
+            }
+            return capitals;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Didn't manage to get capital city details");
+            return null;
+        }
+    }
+
+
+    /**
+     * Prints the contents specified of a list of capital cities.
+     *
+     * @param capitals
+     */
+    public void printCapitals(ArrayList<City> capitals)
+    {
+        // Check cities is not null
+        if (capitals == null)
+        {
+            System.out.println("No cities");
+            return;
+        }
+        // Print header
+        System.out.println(String.format("%-30s %-14s %-20s", "name", "country code", "population"));
+        // Loop over all countries in the list
+        for (City capital : capitals)
+        {
+            if (capital == null)
+                continue;
+            String emp_string =
+                    String.format("%-30s %-14s %-20s",
+                            capital.name, capital.countrycode, capital.population);
+            System.out.println(emp_string);
+        }
+    }
+
+    /**
+     * Prints the contents specified of a list of countries.
+     *
+     * @param cities
+     */
+    public void printCities(ArrayList<City> cities)
+    {
+        // Check cities is not null
+        if (cities == null)
+        {
+            System.out.println("No cities");
+            return;
+        }
+        // Print header
+        System.out.println(String.format("%-30s %-14s %-20s %-8s", "name", "country code", "district", "population"));
+        // Loop over all countries in the list
+        for (City city : cities)
+        {
+            if (city == null)
+                continue;
+            String emp_string =
+                    String.format("%-30s %-14s %-20s %-8s",
+                            city.name, city.countrycode, city.district, city.population);
+            System.out.println(emp_string);
+        }
+    }
+
 }
+
