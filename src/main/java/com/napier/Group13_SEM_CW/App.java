9cd6a4439766267055c52631739aa780fba85d84
Daniel van Dijke <40508135@live.napier.ac.uk>
2022-03-17 15:02:38 +0000

    Package restructuring to remove clutter from app.java

--- src/main/java/com/napier/Group13_SEM_CW/App.java
+++ src/main/java/com/napier/Group13_SEM_CW/App.java
@@ -1,18 +1,22 @@
 package com.napier.Group13_SEM_CW;
 
-
+import com.napier.Group13_SEM_CW.database.Connections;
 import java.sql.*;
 import java.util.ArrayList;
 
 public class App
 {
+    private static Connection con = null;
+
     public static void main(String[] args)
     {
         // Create new Application
         App a = new App();
 
         // Connect to database
-        a.connect();
+        Connections c = new Connections();
+
+        con = c.connect();
 
         //ArrayList<Country> countries = a.displayCountries();
 
@@ -37,75 +41,10 @@ public class App
         //a.printCities(cities4);
 
         // Disconnect from database
-        a.disconnect();
+        c.disconnect();
 
 
     }
-        /**
-         * Connection to MySQL database.
-         */
-        private Connection con = null;
-
-        /**
-         * Connect to the MySQL database.
-         */
-        public void connect()
-        {
-            try
-            {
-                // Load Database driver
-                Class.forName("com.mysql.cj.jdbc.Driver");
-            }
-            catch (ClassNotFoundException e)
-            {
-                System.out.println("Could not load SQL driver");
-                System.exit(-1);
-            }
-
-            int retries = 10;
-            for (int i = 0; i < retries; ++i)
-            {
-                System.out.println("Connecting to database...");
-                try
-                {
-                    // Wait a bit for db to start
-                    Thread.sleep(30000);
-                    // Connect to database
-                    con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-                    System.out.println("Successfully connected");
-                    break;
-                }
-                catch (SQLException sqle)
-                {
-                    System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                    System.out.println(sqle.getMessage());
-                }
-                catch (InterruptedException ie)
-                {
-                    System.out.println("Thread interrupted? Should not happen.");
-                }
-            }
-        }
-
-        /**
-         * Disconnect from the MySQL database.
-         */
-        public void disconnect()
-        {
-            if (con != null)
-            {
-                try
-                {
-                    // Close connection
-                    con.close();
-                    System.out.println("disconnected");
-                }
-                catch (Exception e)
-                {
-                    System.out.println("Error closing connection to database");
-                }
-            }
-        }
 
     /** Use case 10. Gets the top N populated cities in a continent where N is provided by the user.
      *
---
+++ src/main/java/com/napier/Group13_SEM_CW/database/Connections.java
@@ -0,0 +1,72 @@
+package com.napier.Group13_SEM_CW.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connections{
+
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    public Connection connect()
+    {
+        try
+        {
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                return con;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+                System.out.println("disconnected");
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
+    }
+}
