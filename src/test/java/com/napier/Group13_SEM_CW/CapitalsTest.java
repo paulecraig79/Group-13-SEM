d1564ecd246b14ab67b7427b797e59ccb35760c9
Daniel van Dijke <40508135@live.napier.ac.uk>
2022-03-19 17:59:12 +0000

    Unit testing for printCapitals() method

---
+++ src/test/java/com/napier/Group13_SEM_CW/CapitalsTest.java
@@ -0,0 +1,56 @@
+package com.napier.Group13_SEM_CW;
+
+import com.napier.Group13_SEM_CW.reports.Capitals;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CapitalsTest
+{
+    static Capitals capital;
+
+    @BeforeAll
+    static void init()
+    {
+        capital = new Capitals();
+    }
+
+    @Test
+    void printCapitalsTestNull()
+    {
+        capital.printCapitals(null);
+    }
+
+    @Test
+    void printCapitalsTestEmpty()
+    {
+        ArrayList<City> capitals = new ArrayList<>();
+        capital.printCapitals(capitals);
+    }
+
+    @Test
+    void printCapitalsTestsContainsNull()
+    {
+        ArrayList<City> capitals = new ArrayList<>();
+        capitals.add(null);
+        capital.printCapitals(capitals);
+    }
+
+    @Test
+    void printCapitals()
+    {
+        ArrayList<City> capitals = new ArrayList<>();
+        City capital1 = new City();
+        capital1.name = "Cario";
+        capital1.district = "Kairo";
+        capital1.countrycode = "EGY";
+        capital1.population = 67894979;
+        capitals.add(capital1);
+        capital.printCapitals(capitals);
+    }
+}
+
