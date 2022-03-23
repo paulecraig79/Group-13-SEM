4eda4db1a403ff0d2780477a03039be437bf46d2
Daniel van Dijke <40508135@live.napier.ac.uk>
2022-03-23 15:30:21 +0000

    setting up integration testing.

--- src/test/java/com/napier/Group13_SEM_CW/AppIntegrationTest.java
+++ src/test/java/com/napier/Group13_SEM_CW/AppIntegrationTest.java
@@ -1,4 +1,24 @@
 package com.napier.Group13_SEM_CW;
 
+import com.napier.Group13_SEM_CW.database.Connections;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AppIntegrationTest {
+
+    static App app;
+    static Connections c = new Connections();
+
+    @BeforeAll
+    static void init() {
+        app = new App();
+        c.newConnect("localhost:33060", 30000);
+
+    }
 }
