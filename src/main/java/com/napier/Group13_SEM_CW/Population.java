21b19d54117361937026fe5ef2a3c1b512a9bc6e
paulecraig79 <77742997+paulecraig79@users.noreply.github.com>
2022-04-28 10:41:05 +0100

    Adding population class type

---
+++ src/main/java/com/napier/Group13_SEM_CW/Population.java
@@ -0,0 +1,9 @@
+package com.napier.Group13_SEM_CW;
+
+public class Population {
+    public String name;
+    public int population;
+    public int cityPopulation;
+    public int notCityPopulation;
+
+}
